106eadf1b8bec8b28c9c8853caca8aabb2038c84
MixFon <mixfon@rambler.ru>
2021-07-23 11:12:01 +0300

    norma ft_ssl.h md5.h sha256.h

--- include/ft_ssl.h
+++ include/ft_ssl.h
@@ -1,22 +1,22 @@
 #ifndef FT_SSL_H
-#define FT_SSL_H
+# define FT_SSL_H
 
-#include <sys/stat.h>
-#include "md5.h"
-#include "sha256.h"
-#include "libft.h"
+# include <sys/stat.h>
+# include "md5.h"
+# include "sha256.h"
+# include "libft.h"
 
 # define UINT_MAX   0xFFFFFFFF
-# define MAX_LEN    0xFFFFFFFFFFFFFFFF
+# define MAX_LEN	0xFFFFFFFFFFFFFFFF
 
-# define P          0
-# define Q          1
-# define R          2
-# define S          3
+# define P			0
+# define Q			1
+# define R			2
+# define S			3
 
-# define BUFF       4096
+# define BUFF		4096
 
-typedef struct  s_alg
+typedef struct s_alg
 {
 	t_uchar	*(*algorithm)(const t_uchar *data, size_t count_octets);
 	char	*name;
@@ -24,28 +24,26 @@ typedef struct  s_alg
 	int		size;
 }				t_alg;
 
-typedef struct  s_ssl
+typedef struct s_ssl
 {
-    int         flags[4];
-    char        **strings;
-    size_t      count_strings;
-    char        **file_names;
-    size_t      count_file_names;
-    t_uchar     *hash;
-    //char        *name_algorithm;
-    size_t      len_message_oct;
-	int			i;
-	int			k;
-	t_alg		*alg;
-}               t_ssl;
+	int		flags[4];
+	char	**strings;
+	size_t	count_strings;
+	char	**file_names;
+	size_t	count_file_names;
+	t_uchar	*hash;
+	size_t	len_message_oct;
+	int		i;
+	int		k;
+	t_alg	*alg;
+}			t_ssl;
 
-typedef union       u_elem
+typedef union u_elem
 {
-    unsigned int    i_elem;
-    unsigned char   c_elem[4];
-}                   t_elem;
+	unsigned int	i_elem;
+	unsigned char	c_elem[4];
+}					t_elem;
 
 t_alg	*get_algorithms(void);
-//t_uchar *alg_md5(const t_uchar *data, size_t count_octets)
 
-#endif /* ft_ssl_h */
+#endif
--- include/md5.h
+++ include/md5.h
@@ -1,39 +1,36 @@
-
 #ifndef MD5_H
-#define MD5_H
+# define MD5_H
 
-#include "stdio.h"
-#include "libft.h"
+# include "stdio.h"
+# include "libft.h"
 
-# define SIZE_MD5       64
+# define SIZE_MD5	   64
 # define LEN_HASH_MD5   16
 
-# define A          0
-# define B          1
-# define C          2
-# define D          3
-# define F          4
-# define G          5
+# define A		  0
+# define B		  1
+# define C		  2
+# define D		  3
+# define F		  4
+# define G		  5
 
-# define ABS(x) ((x)<0?-(x):(x))
+typedef unsigned int		t_uint;
+typedef unsigned char		t_uchar;
+typedef unsigned long long	t_ulong;
 
-typedef unsigned int        t_uint;
-typedef unsigned char       t_uchar;
-typedef unsigned long long  t_ulong;
-
-typedef struct      s_md
+typedef struct s_md
 {
-    const t_uchar   *data;
-    t_uint          arr_k[SIZE_MD5];
-    t_uint          arr_s[SIZE_MD5];
-    size_t          count_octets;
-    t_uint          hash[4];
-    t_uint          vars[6];
-    t_uint          f;
-    void            (*stages[4])(t_uint *vars, int i);
-}                   t_md;
+	const t_uchar	*data;
+	t_uint			arr_k[SIZE_MD5];
+	t_uint			arr_s[SIZE_MD5];
+	size_t			count_octets;
+	t_uint			hash[4];
+	t_uint			vars[6];
+	t_uint			f;
+	void			(*stages[4])(t_uint *vars, int i);
+}					t_md;
 
-void    print_bits(t_uchar *str, size_t len);
-t_uchar *alg_md5(const t_uchar *data, size_t count_octets);
+void	print_bits(t_uchar *str, size_t len);
+t_uchar	*alg_md5(const t_uchar *data, size_t count_octets);
 
 #endif
